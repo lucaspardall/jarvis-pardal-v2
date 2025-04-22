de1a5a72ff40160ad618572c4f6e6fc06bbb203e
lucaspardall <llouback4@gmail.com>
2025-04-22 00:00:16 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-openai_api_key = os.getenv('OPENAI_API_KEY')
-webhook_make = os.getenv('WEBHOOK_MAKE')
+openai_api_key = os.getenv("OPENAI_API_KEY")
+webhook_make = os.getenv("WEBHOOK_MAKE")
