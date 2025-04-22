4d224e51ef28f183323ec2290fc36f45b2a4c491
lucaspardall <llouback4@gmail.com>
2025-04-22 11:59:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,13 @@ import json
 app = Flask(__name__, static_folder='static')
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
-# Configuração GPT-4.1
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Configuração Firebase via variável de ambiente
 firebase_json = os.getenv("FIREBASE_CONFIG")
 cred = credentials.Certificate(json.loads(firebase_json))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Serve o Painel
 @app.route("/", methods=["GET"])
 def serve_index():
     return app.send_static_file('index.html')
